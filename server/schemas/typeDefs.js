dc5d5da6ed018159fb551e13ac158ed3334c8ccc
stephaniecjohansson <7steph7@gmail.com>
2021-10-07 16:56:40 -0600

    serverSchema

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String!
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  input savedBook {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Query {
+    me: User
+  }
+  type Auth {
+    token: String!
+    user: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+module.exports = typeDefs;
